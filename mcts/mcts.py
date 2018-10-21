726d9f2cb53c9f781900a395c49aaa879b1321e8
Centauria <centauria_v_c@163.com>
2018-10-22 00:18:23 +0800

    the tree got better

--- mcts/mcts.py
+++ mcts/mcts.py
@@ -15,25 +15,30 @@ class Node:
 			self.depth = depth
 		self.index = 0
 		self.state = None
+		self.explored = False
 		self.reward = 0
 		self.time = 0
-		self.children = None
+		self._children = None
 
 	def __str__(self):
 		prepend = TREE_HEAD * self.depth
-		return prepend + '[%i, %.3f, %d]\n' % (self.index, self.reward, self.time)
+		return prepend + '[%i, %.3f, %d]' % (self.index, self.reward, self.time)
 
 	def add_child(self, child: 'Node' = None):
 		if not child:
 			child = Node(self)
-		if not self.children:
-			self.children = Layer(0, self)
-		self.children.add_node(child)
+		if not self._children:
+			self._children = Layer(0, self)
+		self._children.add_node(child)
+
+	@property
+	def children(self):
+		return self._children.nodes
 
 
 class Layer:
 	"""
-	A set of _nodes which have the same parent
+	A set of Node which have the same parent
 	"""
 
 	def __init__(self, length, parent: 'Node' = None):
@@ -42,7 +47,7 @@ class Layer:
 			self._nodes[i] = Node(parent)
 		self.parent = parent
 		if self.parent:
-			self.parent.children = self
+			self.parent._children = self
 			self.depth = self.parent.depth + 1
 		else:
 			self.depth = 0
@@ -59,6 +64,12 @@ class Layer:
 		return len(self._nodes)
 
 	def add_node(self, node: 'Node' = None):
+		"""
+		Add a node at the last index of the Layer.
+		if the node is None, it will create a new Node.
+		:param node: Node, None
+		:return: node
+		"""
 		if not node:
 			node = Node(self.parent)
 		node.parent = self.parent
@@ -87,22 +98,43 @@ class Tree:
 		node = self.root
 		return self._str_iter_dfs(node)
 
-	def _str_iter_dfs(self, node, string=''):
+	def print(self):
+		self._iter_dfs(self.root, lambda node: print(node), (self.root,))
+
+	def _iter_dfs(self, node: 'Node', func: 'function', arg: 'tuple'):
+		func(*arg)
+		if node._children:
+			self._iter_dfs(self.child(node), func, (self.child(node),))
+		if self.sibling(node):
+			self._iter_dfs(self.sibling(node), func, (self.sibling(node),))
+
+	def _str_iter_dfs(self, node: 'Node', string=''):
 		result = string + str(node)
-		if node.children:
+		if node._children:
 			result += self._str_iter_dfs(self.child(node), string)
 		if self.sibling(node):
 			result += self._str_iter_dfs(self.sibling(node), string)
 		return result
 
 	def child(self, node: 'Node', index: 'int>=0' = 0):
-		return node.children.nodes[index]
+		return node.children[index]
 
 	def sibling(self, node: 'Node'):
-		if node.parent and node.index < node.parent.children.length - 1:
-			return node.parent.children.nodes[node.index + 1]
+		if node.parent and node.index < node.parent._children.length - 1:
+			return node.parent.children[node.index + 1]
 		else:
 			return None
+
+	def reset(self):
+		"""
+		Set the variable 'explored' of all Node in the Tree to False.
+		:return: None
+		"""
+
+		def set_to_false(node):
+			node.explored = False
+
+		self._iter_dfs(self.root, set_to_false, (self.root,))
 
 	def select(self):
 		pass
@@ -133,11 +165,9 @@ def test():
 	t.root.add_child()
 	t.root.add_child()
 	t.root.add_child()
-	t.root.children.nodes[0].add_child()
-	t.root.children.nodes[0].add_child()
-	t.root.children.nodes[0].add_child()
-	t.root.children.nodes[0].add_child()
-	print(t)
+	t.root.children[2].add_child()
+	t.print()
+	pass
 
 
 if __name__ == '__main__':
